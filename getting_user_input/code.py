a341ebbd2cf0515fa43fd32730ff0eaa1a2c0a2d
rasyidannas <rasshit.dsgn@gmail.com>
2025-05-23 20:34:47 +0700

    2/8. String formatting in Python

---
+++ getting_user_input/code.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+print(name)
+
+size_input = input("How big is your house (in square meters): ")
+square_meters = int(size_input)
+square_feet = square_meters * 10.8
+print(square_feet)
