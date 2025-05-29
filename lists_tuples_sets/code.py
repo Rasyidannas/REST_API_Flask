d688b66b3ff29324b2654aaf4b6eaab02a1f9a18
rasyidannas <rasshit.dsgn@gmail.com>
2025-05-29 18:22:58 +0700

    2/11. Lists, tuples and sets

---
+++ lists_tuples_sets/code.py
@@ -0,0 +1,6 @@
+lst = ["Bob", "Rolf", "Anne"] # modifiable, ordered
+tpl = ("Bob", "Rolf", "Anne") # unmodifiable, ordered
+st = {"Bob", "Rolf", "Anne"} # unmodifiable, unique, unordered
+
+print(lst[0])
+print(st[1]) # this is proof if set unordered
