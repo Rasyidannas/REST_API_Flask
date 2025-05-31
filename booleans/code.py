32c40c031a9de98fffb3bcd6942772998e501101
rasyidannas <rasshit.dsgn@gmail.com>
2025-05-31 23:02:49 +0000

    2/14. Booleans in python.mp4

---
+++ booleans/code.py
@@ -0,0 +1,10 @@
+print(5 == 5)
+print(5 > 5)
+
+# Comparisons: ==, !=, >, <, >=, <=
+
+friends = ["Rasyid", "Ciko"]
+abroad = ["Rasyid", "Ciko"]
+
+print(friends == abroad)
+print(friends is abroad) # this will retern False, but if you want it True you should change abroad = friends in declraation
