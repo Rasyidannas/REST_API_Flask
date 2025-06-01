3707d067ff865b1b06df6d1bab76182d5dd9bf98
rasyidannas <rasshit.dsgn@gmail.com>
2025-06-01 01:32:08 +0000

    2/16. The in keyword in Python

---
+++ the_in_keyword/code.py
@@ -0,0 +1,9 @@
+# this work in list
+friends = ["Rolf", "Bob", "Jen"]
+print("Jen" in friends)
+
+# this work in set
+movies_watched = {"The Matrix", "Green Book", "Her"}
+user_movie = input("Enter somethig you've watched recently: ")
+
+print(user_movie in movies_watched)
